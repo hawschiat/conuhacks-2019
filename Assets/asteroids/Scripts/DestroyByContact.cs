2f6b718cf5b3b85b88403cfd5a57ea440fbb55ed
Nuri <nuri.amiraslan@mail.mcgill.ca>
2019-01-26 18:42:10 -0500

    asteroids
    Has a bunch of useless shit will delete after use

---
+++ Assets/asteroids/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    // Use this for initialization
+   void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Boundry")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/asteroids/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    // Use this for initialization
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start () {
+
+        rb.velocity = transform.forward * speed;
+    }
+	
+ 
+}
