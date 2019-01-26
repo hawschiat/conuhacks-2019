00c950d774fee75caab3f57bed9c1b2a36f065e0
Seng Chiat Haw <haw1997@hotmail.com>
2019-01-26 18:11:54 -0500

    spinning works

--- Assets/scripts/Spin.cs
+++ Assets/scripts/Spin.cs
@@ -9,10 +9,10 @@ public class Spin : MonoBehaviour
     {
         
     }
-
+    public float speed;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, speed, 0);
     }
 }
